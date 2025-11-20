bd06c330b9874d8b47fab4e4cbf96b134b7551d3
Tawhid Talal <tawhid.talal@gmail.com>
2025-11-20 10:43:19 -0500

    total courses, total trainers and total students data is provided

--- training_portal/views.py
+++ training_portal/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from academy.models import Course, Trainer, Student
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    total_courses = Course.objects.count()
+    total_trainers = Trainer.objects.count()
+    total_students = Student.objects.count()
+    context = {
+        'total_courses': total_courses,
+        'total_trainers': total_trainers,
+        'total_students': total_students,
+    }
+    return render(request, 'home.html', context)
